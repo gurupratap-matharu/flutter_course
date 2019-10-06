c20ef51d9c36dfb3999dd26d2785884136f046af
Gurupratap <gurupratap.matharu@gmail.com>
2019-10-06 10:44:11 -0300

    adding card widget with two children column widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Easy List'),
+          title: Text('Veer your first app! :D'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image(),
+          Text('Food Paradise')
+        ],),)
       ),
     );
   }
